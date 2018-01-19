89c9a398b2156911d8494f55c4baa8ad658f0187
unknown <aj.dev254@gmail.com>
2018-01-19 22:02:15 +0300

    Server accessing file

---
+++ serverfs.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const fs = require("fs");
+
+//Create the server
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    fs.readFile("./somefile.txt",(err,file)=>{
+        res.end(file);
+    });
+    
+});
+
+//Listen to the port
+server.listen(8000);
